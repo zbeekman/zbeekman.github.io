8e8b2d6b44821ad278ebc27e696d6a673c1ef20b
zbeekman <zbeekman@gmail.com>
2015-08-06 15:53:10 -0400

    Use Formspree for contact form
     - gh-pages doesn't allow CGI serverside scripts
     - php mail script was not working because of that

--- js/contact_me.js
+++ js/contact_me.js
@@ -11,6 +11,7 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
+	    var gotcha = $("input#_gotcha").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -24,7 +25,8 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+		    _gotcha: gotcha
                 },
 		dataType: "json",
                 cache: false,
@@ -34,7 +36,11 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>")
+		    $('#success > .alert-success')
+                        .append(firstName)
+		    $('#success > .alert-success')
+                        .append(", thank you for your message.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
