30240a0bdfbb8f67088faec85ec07a2f7b76e655
zbeekman <zbeekman@gmail.com>
2015-08-06 11:24:31 -0400

    This may fix it

--- js/contact_me.js
+++ js/contact_me.js
@@ -27,30 +27,30 @@ $(function() {
                     message: message
                 },
 		dataType: "json"
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+                // cache: false,
+                // success: function() {
+                //     // Success message
+                //     $('#success').html("<div class='alert alert-success'>");
+                //     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                //         .append("</button>");
+                //     $('#success > .alert-success')
+                //         .append("<strong>Your message has been sent. </strong>");
+                //     $('#success > .alert-success')
+                //         .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
+                //     //clear all fields
+                //     $('#contactForm').trigger("reset");
+                // },
+                // error: function() {
+                //     // Fail message
+                //     $('#success').html("<div class='alert alert-danger'>");
+                //     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                //         .append("</button>");
+                //     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                //     $('#success > .alert-danger').append('</div>');
+                //     //clear all fields
+                //     $('#contactForm').trigger("reset");
+                // },
             })
         },
         filter: function() {
