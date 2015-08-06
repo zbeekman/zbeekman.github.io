7a608ce05d68a1eb27065d62506af7cfc17734f1
zbeekman <zbeekman@gmail.com>
2015-08-06 11:15:23 -0400

    Use FORMSPREE since php won't run on GH pages

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,14 +18,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "//formspree.io/zbeekman@gmail.com",
+                method: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message
                 },
+		dataType: "json"
                 cache: false,
                 success: function() {
                     // Success message
